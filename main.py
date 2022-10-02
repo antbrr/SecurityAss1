3d4e69f80893ce8c0e3eaa9280f9f9d9b7c8bb6a
antbrr <antbr@itu.dk>
2022-10-02 12:48:51 +0200

    Assignment donezio

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def bruteforce(pk):
         posPK = genPk(g, i)
         if posPK == pk:
             return i
-    raise Exception("WTF MAN!?")
+    raise Exception("Could not bruteforce the sk")
 
 
 def encrypt(m, pkRec):
@@ -34,23 +34,20 @@ p = 6661  # Share prime
 bobPK = 2227
 aliceSK = genSk()
 alicePK = genPk(aliceSK, g)
-mallorySK = genSk()
-malloryPK = genPk(g, mallorySK)
 
 
 def main():
-    # Ass 1.1 & Ass 1.2
+    # Ass 1.1
     (c1, c2) = encrypt(2000, bobPK)
     print("c1 value is " + str(c1))
     print("c2 value is " + str(c2))
+
+    # Ass 1.2
     bobSK = bruteforce(bobPK)
     print("Bobs secret key is " + str(bobSK))
     print("The message from Alice is " + str(decrypt(c1, c2, bobSK)))
 
     # Ass 1.3
-    global alicePK
-    alicePK = malloryPK
-
     (badc1, badc2) = encrypt(6000, bobPK)
     c1 = badc1
     c2 = badc2
